b2d84c4cdf7dc0ef43948425df728c20a46db421
yixinl3 <liao102@purdue.edu>
2022-03-22 23:45:15 -0400

    Update favorite and home content

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,49 +1,48 @@
-import React from 'react';
-import { Menu, Button, Drawer, message } from 'antd';
-import { EyeOutlined, YoutubeOutlined, VideoCameraOutlined, StarFilled } from '@ant-design/icons';
-import { getFavoriteItem } from '../utils';
- 
+import React from "react";
+import { Menu, Button, Drawer } from "antd";
+import {
+  EyeOutlined,
+  YoutubeOutlined,
+  VideoCameraOutlined,
+  StarFilled,
+} from "@ant-design/icons";
+
 const { SubMenu } = Menu;
 const MenuKey = {
-  Streams: 'streams',
-  Videos: 'videos',
-  Clips: 'clips'
-}
+  Streams: "streams",
+  Videos: "videos",
+  Clips: "clips",
+};
 class Favorites extends React.Component {
   state = {
     displayDrawer: false,
-    data: {
-      VIDEO: [],
-      STREAM: [],
-      CLIP: [],
-    }
-  }
- 
+  };
+
   onDrawerClose = () => {
     this.setState({
       displayDrawer: false,
-    })
-  }
- 
+    });
+  };
+
   onFavoriteClick = () => {
-    getFavoriteItem()
-      .then((data) => {
-        this.setState({
-          data,
-          displayDrawer: true,
-        })
-      }).catch((err) => {
-        message.error(err.message);
-      })
-  }
- 
+    this.setState({
+      displayDrawer: true,
+    });
+  };
+
   render = () => {
-    const { VIDEO, STREAM, CLIP } = this.state.data;
- 
+    const { VIDEO, STREAM, CLIP } = this.props.data;
+
     return (
       <>
-        <Button type="primary" shape="round" onClick={this.onFavoriteClick} icon={<StarFilled />}>
-          My Favorites</Button>
+        <Button
+          type="primary"
+          shape="round"
+          onClick={this.onFavoriteClick}
+          icon={<StarFilled />}
+        >
+          My Favorites
+        </Button>
         <Drawer
           title="My Favorites"
           placement="right"
@@ -54,53 +53,71 @@ class Favorites extends React.Component {
           <Menu
             mode="inline"
             defaultOpenKeys={[MenuKey.Streams]}
-            style={{ height: '100%', borderRight: 0 }}
+            style={{ height: "100%", borderRight: 0 }}
             selectable={false}
           >
-            <SubMenu key={MenuKey.Streams} icon={<EyeOutlined />} title="Streams">
-              {
-                STREAM.map((item) => {
-                  return (
-                    <Menu.Item key={item.id}>
-                      <a href={item.url} target="_blank" rel="noopener noreferrer">
-                        {`${item.broadcaster_name} - ${item.title}`}
-                      </a>
-                    </Menu.Item>
-                  )
-                })
-              }
+            <SubMenu
+              key={MenuKey.Streams}
+              icon={<EyeOutlined />}
+              title="Streams"
+            >
+              {STREAM.map((item) => {
+                return (
+                  <Menu.Item key={item.id}>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {`${item.broadcaster_name} - ${item.title}`}
+                    </a>
+                  </Menu.Item>
+                );
+              })}
             </SubMenu>
-            <SubMenu key={MenuKey.Videos} icon={<YoutubeOutlined />} title="Videos">
-              {
-                VIDEO.map((item) => {
-                  return (
-                    <Menu.Item key={item.id}>
-                      <a href={item.url} target="_blank" rel="noopener noreferrer">
-                        {`${item.broadcaster_name} - ${item.title}`}
-                      </a>
-                    </Menu.Item>
-                  )
-                })
-              }
+            <SubMenu
+              key={MenuKey.Videos}
+              icon={<YoutubeOutlined />}
+              title="Videos"
+            >
+              {VIDEO.map((item) => {
+                return (
+                  <Menu.Item key={item.id}>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {`${item.broadcaster_name} - ${item.title}`}
+                    </a>
+                  </Menu.Item>
+                );
+              })}
             </SubMenu>
-            <SubMenu key={MenuKey.Clips} icon={<VideoCameraOutlined />} title="Clips">
-              {
-                CLIP.map((item) => {
-                  return (
-                    <Menu.Item key={item.id}>
-                      <a href={item.url} target="_blank" rel="noopener noreferrer">
-                        {`${item.broadcaster_name} - ${item.title}`}
-                      </a>
-                    </Menu.Item>
-                  )
-                })
-              }
+            <SubMenu
+              key={MenuKey.Clips}
+              icon={<VideoCameraOutlined />}
+              title="Clips"
+            >
+              {CLIP.map((item) => {
+                return (
+                  <Menu.Item key={item.id}>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {`${item.broadcaster_name} - ${item.title}`}
+                    </a>
+                  </Menu.Item>
+                );
+              })}
             </SubMenu>
           </Menu>
         </Drawer>
       </>
-    )
-  }
+    );
+  };
 }
- 
+
 export default Favorites;
